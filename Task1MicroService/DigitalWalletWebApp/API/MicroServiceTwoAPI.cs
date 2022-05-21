0cd20e5fc59fe7d19f0553a56ae0b9550f626825
AdrianPortelli <adrian@keen-advertising.com>
2022-05-21 19:01:21 +0200

    Added webClient to Web App and started implention of MicroServiceTwo integration

---
+++ Task1MicroService/DigitalWalletWebApp/API/MicroServiceTwoAPI.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+using DigitalWalletWebApp.WebClient;
+
+
+namespace MicroServiceSix.API
+{
+    public class MicroServiceTwoAPI
+    {
+        
+      /*  private readonly string hostEndpoint = "https://api.apilayer.com/exchangerates_data";
+        private readonly string lastestendpoint = "latest";*/
+
+
+
+        private readonly webClient webclient = webClient.WebClientInstance;
+
+        private static MicroServiceTwoAPI exchangeRateApi = null;
+
+        private MicroServiceTwoAPI() { }
+
+
+        public static MicroServiceTwoAPI ExchangeRateAPI
+        {
+            get
+            {
+                if(exchangeRateApi == null)
+                {
+                    exchangeRateApi = new MicroServiceTwoAPI();
+                }
+                return exchangeRateApi;
+            }
+        }
+
+
+
+        private HttpRequestMessage GetRequestMessage(string hostEndpoint,string microServiceControllerName,string endpoint,string parameters) { 
+
+            var request = new HttpRequestMessage
+            {
+                Method = HttpMethod.Get,
+                RequestUri = new Uri(hostEndpoint + "/api/v1/TaskTwo/"+ endpoint + "/"+ parameters),
+                
+            };
+
+            return request;
+        }
+
+
+
+        // parsing request
+    }
+}
---
+++ Task1MicroService/DigitalWalletWebApp/WebClient/webClient.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace DigitalWalletWebApp.WebClient
+{
+    public class webClient
+    {
+
+
+        private static webClient webClientInstance = null;
+
+        private webClient() { }
+
+        public static webClient WebClientInstance
+        {
+            get
+            {
+                if (webClientInstance == null)
+                {
+                    webClientInstance = new webClient();
+                }
+
+                return webClientInstance;
+            }
+        }
+
+        public async Task<JsonDocument> Request(HttpRequestMessage requestMessage)
+        {
+            string responseValue;
+
+            var client = new HttpClient();
+
+            using (var response = await client.SendAsync(requestMessage))
+            {
+                response.EnsureSuccessStatusCode();
+                var body = await response.Content.ReadAsStringAsync();
+                responseValue = body;
+            }
+
+            return JsonDocument.Parse(responseValue);
+        }
+    }
+}
